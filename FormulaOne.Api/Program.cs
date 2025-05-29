fe7ff3f4e4e1100060a206ff64535f38397cbf5e
Cesar <cesarlerdeut@gmail.com>
2025-05-29 10:06:20 +0200

    Adding minimal API to deploy

---
+++ FormulaOne.Api/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var bestPilots = new[]
+{
+    "Checo", "Alonso", "Senna", "LeClerc", "Sainz", "Colapinto"
+};
+
+app.MapGet("/best-pilots", () => bestPilots)
+.WithName("GetBestPilots");
+
+app.Run();
