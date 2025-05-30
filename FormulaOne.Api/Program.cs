924e5cc9187120c6bc0c321233bd93121b7be7f8
Cesar <cesarlerdeut@gmail.com>
2025-05-30 09:10:31 +0200

    Fix logs

--- FormulaOne.Api/Program.cs
+++ FormulaOne.Api/Program.cs
@@ -19,7 +19,17 @@ var bestPilots = new[]
     "Checo", "Alonso", "Senna", "LeClerc", "Sainz", "Colapinto"
 };
 
-app.MapGet("/best-pilots", () => bestPilots)
+app.MapGet("/api/best-pilots", (ILogger<Program> logger) =>
+{
+    logger.LogInformation("Fetching the best pilots in Formula 1");
+    var pilots = bestPilots.Select(pilot => new { Name = pilot }).ToList();
+    var result = new
+    {
+        Pilots = pilots,
+        Count = pilots.Count
+    };
+    return Results.Ok(result);
+})
 .WithName("GetBestPilots");
 
 app.Run();
